bc7a19763e86871a8aa26c237ee91a12055dfbc1
shavronskyy <dima.shavronskyi@gmail.com>
2024-07-10 16:22:55 +0300

    add proj

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const subHeaderStyle = {
+    color: "blueviolet",
+    backgroundColor: "lightgray"
+};
+
+function Header(){
+    return (
+    <div>
+        <MainHeader />
+        <SubHeader />
+    </div>);
+}
+
+function MainHeader(){
+    return <h1 className='text-primary'>React course!</h1>
+}
+
+function SubHeader(){
+    return <p style={subHeaderStyle}>This will be an exciting course.</p>
+}
+
+function MainBody(){
+    return (
+        <div>
+            <p>In this course, we will learn react js by building TaskOPedia</p>
+            <ul>
+                <li>Call Ben</li>
+                <li>Go to walmart</li>
+            </ul>
+        </div>
+    );
+}
+
+function Footer(){
+    return <p style={{color:'gray', backgroundColor:'black'}}>Happy coding!</p>
+}
+
+root.render(
+    <div>
+        <Header />
+        <MainBody />
+        <Footer />
+    </div>
+);
